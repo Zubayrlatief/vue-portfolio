6c22ade7eae1198f0746acd2327afd18c3973508
Zubayr-Latief <zubayrlatie007@gmail.com>
2024-07-04 11:15:07 +0200

    m

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,12 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
   aboutMe :null,
-    :null,
-    :null,
-    :null,
-    :null,
-    :null,
-    :null,
+  projects :null,
+  eductaion :null,
+  experience :null,
+  codingSkills  :null,
+  softSkills :null,
+  testimonials :null,
 
   },
   getters: {
@@ -20,15 +20,38 @@ export default createStore({
     setAboutMe (state,payload){
       state.aboutMe = payload
     },
+    setProjects (state,payload){
+      state.projects = payload
+    },
+     setEducation (state,payload){
+       state.eductaion = payload
+    },
+    setExperience (state,payload){
+       state.experience = payload
+    },
+    setCodingSkills (state,payload){
+       state.codingSkills = payload
+    },
+    setSoftSkills (state,payload){
+        state.softSkills = payload
+    },
+    setTestimonials (state,payload){
+        state.testimonials = payload
+     },
   },
   actions: {
     async getAboutMe ({commit}) {
       let fetchInfo = await fetch('https://zubayrlatief.github.io/JsonFileForPortfolio/Data/data.json')
       let data = await fetchInfo.json()
-      let (aboutMe, projects ,eductaion,) = data
+      let {aboutMe, projects ,eductaion, experience, codingSkills, softSkills, testimonials} = data
       console.log(data)
       commit('setAboutMe', aboutMe)
       commit('setProjects', projects)
+      commit('setEduction', eductaion)
+      commit('setExperience', experience)
+      commit('setCodingSkills', codingSkills)
+      commit('setsoftSkills', softSkills)
+      commit('setTestimonials', testimonials)
     },
   },
   modules: {
